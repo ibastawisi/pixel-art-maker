f923047d1319419e9d299c39433dc5a8ebe4d0fe
ibrahim-1996 <ibrahim.1996@outlook.com>
2018-03-18 23:57:18 +0200

    Paint white if clicked again

--- designs.js
+++ designs.js
@@ -35,7 +35,17 @@ submit.addEventListener('click', function (event) {
     makeGrid(inputWidth.value, inputHeight.value);
 });
 
+// hex to rgb function
+hex2rgb = function (hex) {
+    hex = hex.replace('#', '');
+    r = parseInt(hex.substring(0, 2), 16);
+    g = parseInt(hex.substring(2, 4), 16);
+    b = parseInt(hex.substring(4, 6), 16);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
 // canvas click event
 canvas.addEventListener('click', function (event) {
-    if (event.target.nodeName === 'TD') { event.target.style.backgroundColor = colorPicker.value; }
+    if (event.target.nodeName === 'TD') { event.target.style.backgroundColor =
+        event.target.style.backgroundColor === hex2rgb(colorPicker.value) ? '#fff' : colorPicker.value; }
 });
